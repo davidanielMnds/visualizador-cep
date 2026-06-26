2dc5f612118b7af04f5a2e3aa2c808e6f5f1e90e
Davi Daniel <davidaniel.2k26@gmail.com>
2026-06-26 20:19:53 -0300

    criar arquivos principais

---
+++ src/main/java/com/d4igen/visualizar_cep/VisualizarCepApplication.java
@@ -0,0 +1,13 @@
+package com.d4igen.visualizar_cep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizarCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizarCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d4igen/visualizar_cep/VisualizarCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d4igen.visualizar_cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizarCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
