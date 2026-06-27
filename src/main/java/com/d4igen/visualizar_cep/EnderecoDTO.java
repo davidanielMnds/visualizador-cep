86a4f81c39b0a33c50b8a9a30687f0d6637dc36f
Davi Daniel <davidaniel.2k26@gmail.com>
2026-06-27 14:40:02 -0300

    feat: adcionar classe EnderecoDTO

---
+++ src/main/java/com/d4igen/visualizar_cep/EnderecoDTO.java
@@ -0,0 +1,15 @@
+package com.d4igen.visualizar_cep;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EnderecoDTO {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+
+}
